4c5d2284adebd46369eba362db58bc9eb0800daf
Gan2Kirich <kiriltchekulae@yandex.ru>
2026-05-03 21:11:58 +0300

    Add files via upload
    Задачи с 1.7 по 1.10

---
+++ Pythontasks17-110/Pythontasks17-110/Pythontasks17_110.py
@@ -0,0 +1,21 @@
+# Задача 1.7
+'''
+import math
+e =math.e
+print(f"{e:.1f}")
+'''
+# Задача 1.8
+'''
+asd = input("Vv: ")
+print(f"Name: {asd}")
+'''
+# Задача 1.9
+'''
+asd = input("Vv: ")
+print(f"{asd} - This is the number you entered")
+'''
+# Задача 1.10
+'''
+asd = input("Enter your name: ")
+print(f"Hello {asd} ")
+'''
