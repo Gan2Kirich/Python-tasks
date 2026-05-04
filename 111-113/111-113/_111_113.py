430b536c4f13324a453c5a4bd96de52560aca5a1
Gan2Kirich <kiriltchekulae@yandex.ru>
2026-05-04 22:40:52 +0300

    Add files via upload
    Задачи с 1.11 по 1.13

---
+++ 111-113/111-113/_111_113.py
@@ -0,0 +1,18 @@
+# Задача 1.11
+'''
+asd = input("Enter the name of the football team: ")
+print(f"{asd} - This is the champion")
+'''
+# Задача 1.12
+'''
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+'''
+# Задача 1.13
+'''
+n = int(input("enter a number: "));
+c = n - 1;
+d = n + 1;
+print(f"Next to the number {n} number {d}");
+print(f"For the number {n} previous number {c}");
+'''
